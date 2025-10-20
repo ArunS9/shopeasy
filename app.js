bb13a0d7b649153f3abfe89827fa994c284bf020
ArunS9 <arunsubu2003@gmail.com>
2025-10-20 11:50:57 +0530

    Change in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 443;
 
 app.get("/", (req, res) => {
-    res.send("Welcome to ShopEasy!");
+    res.send("Welcome to ShopEasy from Arun!");
 })
 
 app.listen(PORT, () => {
